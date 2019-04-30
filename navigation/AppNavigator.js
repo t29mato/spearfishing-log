5a346cecc1592717c70060154c99a91d476bf582
mato <tomoya.matou@gmail.com>
2019-05-01 04:35:49 +0900

    refactor: separate master data from model

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -43,7 +43,7 @@ export default createAppContainer(
       WeatherSelectScreen,
     },
     {
-      initialRouteName: 'MainBottomTabNavigator',
+      initialRouteName: 'CatchCreateScreen',
     }
   )
 );
